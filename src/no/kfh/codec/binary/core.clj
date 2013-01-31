66439d7c56d3fbe23f19d61e82d0261a972fe84f
Kristian Frøhlich Hansen <kfh@kfh.no>
2013-02-01 00:10:26 +0100

    initial import

---
+++ src/no/kfh/codec/binary/core.clj
@@ -0,0 +1,26 @@
+(ns no.kfh.codec.binary.core
+    (:use [clojure.math.numeric-tower :only [expt]]))
+
+(def base62-string "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+(defn encode [base-string number]
+    (apply str
+        (for [x (loop [xs '() my-n number]
+            (let [c (count base-string)
+                  r (mod my-n c)
+                  q (quot my-n c)]
+                (if (pos? my-n) (recur (conj xs r) q)
+                    xs)))]
+            (nth base-string x))))
+
+(defn decode [base-string encoded-string]
+    (let [xs (map #(.indexOf base-string (str %)) encoded-string)]
+        (reduce +
+            (map #(* % (expt (count base-string) (.indexOf (reverse xs) %)))
+                xs))))
+
+(def encode-base62 (partial encode base62-string))
+
+(def decode-base62 (partial decode base62-string))
+
+
---
+++ test/no/kfh/codec/binary/core_test.clj
@@ -0,0 +1,7 @@
+(ns no.kfh.codec.binary.core-test
+  (:use clojure.test
+        no.kfh.codec.binary.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
