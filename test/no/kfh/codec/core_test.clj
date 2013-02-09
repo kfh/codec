b89ba5959e788a37d096728feadab18cc1611702
Kristian Frøhlich Hansen <kfh@kfh.no>
2013-02-09 13:58:55 +0100

    added tests

--- test/no/kfh/codec/core_test.clj
+++ test/no/kfh/codec/core_test.clj
@@ -1,7 +1,13 @@
 (ns no.kfh.codec.core-test
-  (:use clojure.test
-        no.kfh.codec.core))
+    (:use clojure.test
+          no.kfh.codec.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-encode-base62
+    (is (= "e9a" (encode-base62 19158)))
+    (is (= "Gzj" (encode-base62 124567)))
+    (is (= "b"   (encode-base62 1))))
+
+(deftest test-decode-base62
+    (is (= 19158  (decode-base62 "e9a")))
+    (is (= 124567 (decode-base62 "Gzj")))
+    (is (= 1      (decode-base62 "b"))))
