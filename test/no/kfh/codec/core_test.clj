a008a3b08969e13d178cf25b7072cd1225613fc9
Kristian Frøhlich Hansen <kfh@kfh.no>
2013-02-09 18:44:15 +0100

    updated tests

--- test/no/kfh/codec/core_test.clj
+++ test/no/kfh/codec/core_test.clj
@@ -2,12 +2,15 @@
     (:use clojure.test
           no.kfh.codec.core))
 
-(deftest test-encode-base62
-    (is (= "e9a" (encode-base62 19158)))
-    (is (= "Gzj" (encode-base62 124567)))
-    (is (= "b"   (encode-base62 1))))
 
-(deftest test-decode-base62
-    (is (= 19158  (decode-base62 "e9a")))
-    (is (= 124567 (decode-base62 "Gzj")))
-    (is (= 1      (decode-base62 "b"))))
+(deftest encode-number
+    (testing "encode numbers to strings"
+        (is (= "e9a" (encode-base62 19158)))
+        (is (= "Gzj" (encode-base62 124567)))
+        (is (= "b" (encode-base62 1)))))
+
+(deftest decode-string
+    (testing "decode strings to numbers "
+        (is (= 19158 (decode-base62 "e9a")))
+        (is (= 124567 (decode-base62 "Gzj")))
+        (is (= 1 (decode-base62 "b")))))
